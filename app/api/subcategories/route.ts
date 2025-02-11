1af11c34bee86d8e210b7e8e79f3adfed4637da2
Roman Bondarenko <93474094+romario999@users.noreply.github.com>
2025-02-11 15:00:58 +0200

    add revalidate subcategories

--- app/api/subcategories/route.ts
+++ app/api/subcategories/route.ts
@@ -1,6 +1,8 @@
 import { prisma } from "@/prisma/prisma-client";
 import { NextResponse } from "next/server";
 
+export const revalidate = 60;
+
 export async function GET() {
     const subcategories = await prisma.subcategory.findMany({
         include: {
