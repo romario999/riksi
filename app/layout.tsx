8b175d346b32487bb94c79dec97225738c0728f0
Roman Bondarenko <93474094+romario999@users.noreply.github.com>
2025-02-17 14:46:37 +0200

    test font optimization

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ const nunito = Nunito({
   subsets: ['cyrillic'],
   variable: '--font-nunito',
   weight: ['400', '500', '600', '700', '800', '900'],
+  preload: true
 });
 
 export default function RootLayout({
