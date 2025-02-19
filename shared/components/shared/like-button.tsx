f5450a7d9f764ada1408537379069da975490c2d
Roman Bondarenko <93474094+romario999@users.noreply.github.com>
2025-02-19 08:43:33 +0200

    fixed scrollbar markup

--- shared/components/shared/like-button.tsx
+++ shared/components/shared/like-button.tsx
@@ -14,22 +14,24 @@ const LikeButton: React.FC<LikeButtonProps> = () => {
   const isDisabled = session?.id ? false : true;
   const {favoriteItems} = useFavorites();
   return (
-    <Link href="/wishlist" >
-      <Button 
-        variant="ghost" 
-        size="icon" 
-        className="relative p-2 hover:bg-gray-50 transition-colors hidden md:block disabled:bg-white"
-        aria-label="Like"
-        disabled={isDisabled}
-      >
-          <Heart size={22} className="text-gray-900 hover:text-black transition-colors" />
-          {favoriteItems.length > 0 && (
-            <span className="absolute -top-1 -right-1 bg-gray-900 text-white text-xs font-bold px-1.5 py-0.5 rounded-full">
-              {favoriteItems.length}
-            </span>
-          )}
-      </Button>
-    </Link>
+    <div className={`${isDisabled && 'pointer-events-none'}`}>
+      <Link href="/wishlist">
+        <Button 
+          variant="ghost" 
+          size="icon" 
+          className="relative p-2 hover:bg-gray-50 transition-colors hidden md:block disabled:bg-white"
+          aria-label="Like"
+          disabled={isDisabled}
+        >
+            <Heart size={22} className="text-gray-900 hover:text-black transition-colors" />
+            {favoriteItems.length > 0 && (
+              <span className="absolute -top-1 -right-1 bg-gray-900 text-white text-xs font-bold px-1.5 py-0.5 rounded-full">
+                {favoriteItems.length}
+              </span>
+            )}
+        </Button>
+      </Link>
+    </div>
   );
 };
 
