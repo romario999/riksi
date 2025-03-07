c32fef8a43a8f374ca47d42b7d1b2afa701a395e
Roman Bondarenko <93474094+romario999@users.noreply.github.com>
2025-03-07 13:23:57 +0200

    add webp logo

--- shared/components/shared/email-templates/verification-user.tsx
+++ shared/components/shared/email-templates/verification-user.tsx
@@ -1,7 +1,6 @@
 'use server';
 
 import React from 'react';
-import { Body, Container, Head, Html, Img, Text } from '@react-email/components';
 
 interface Props {
   code: string;
@@ -9,52 +8,54 @@ interface Props {
 
 export const VerificationUserTemplate: React.FC<Props> = ({ code }) => {
   return (
-    <Html>
-      <Head />
-      <Body style={{ fontFamily: 'sans-serif', backgroundColor: '#edf2f7', height: '100%', padding: '24px' }}>
-        <Container
+    <div
+      style={{
+        fontFamily: 'sans-serif',
+        backgroundColor: '#edf2f7',
+        height: '100%',
+        padding: '24px',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <div
+        style={{
+          backgroundColor: 'white',
+          padding: '40px',
+          borderRadius: '16px',
+          boxShadow: '0 4px 20px rgba(0, 0, 0, 0.1)',
+          margin: '0 auto',
+          maxWidth: '600px',
+          width: '100%',
+          textAlign: 'center',
+        }}
+      >
+        <div style={{ marginBottom: '24px' }}>
+          <img src={`${process.env.NEXT_PUBLIC_SITE_URL}/riksi.webp`} alt="Logo RIKSI" style={{ margin: '0 auto', width: '120px' }} />
+        </div>
+        <h1 style={{ fontSize: '24px', fontWeight: '600', color: 'black', marginBottom: '16px' }}>
+          Підтвердження вашої реєстрації на сайті RIKSI
+        </h1>
+        <p style={{ fontSize: '18px', color: 'black', marginBottom: '24px' }}>
+          Ви отримали цей лист, тому що ми отримали запит на реєстрацію на нашому сайті. Для підтвердження
+          реєстрації, будь ласка, використовуйте наступний код:
+        </p>
+        <div
           style={{
-            backgroundColor: 'white',
-            padding: '40px',
-            borderRadius: '16px',
-            boxShadow: '0 4px 20px rgba(0, 0, 0, 0.1)',
-            margin: '0 auto',
-            maxWidth: '600px',
-            width: '100%',
-            textAlign: 'center',
+            color: 'black',
+            fontSize: '24px',
+            fontWeight: '700',
+            padding: '16px 0',
+            borderRadius: '8px',
           }}
         >
-          <div style={{ marginBottom: '24px' }}>
-            <Img
-              src="https://www.riksi.com.ua/assets/images/riksi.png"
-              alt="Logo RIKSI"
-              width="120"
-              style={{ margin: '0 auto' }}
-            />
-          </div>
-          <h3 style={{ fontSize: '24px', fontWeight: '600', color: 'black', marginBottom: '16px' }}>
-            Підтвердження вашої реєстрації на сайті RIKSI
-          </h3>
-          <Text style={{ fontSize: '18px', color: 'black', marginBottom: '24px' }}>
-            Ви отримали цей лист, тому що ми отримали запит на реєстрацію на нашому сайті. Для підтвердження
-            реєстрації, будь ласка, використовуйте наступний код:
-          </Text>
-          <div
-            style={{
-              color: 'black',
-              fontSize: '24px',
-              fontWeight: '700',
-              padding: '16px 0',
-              borderRadius: '8px',
-            }}
-          >
-            {code}
-          </div>
-          <Text style={{ fontSize: '14px', color: '#a0aec0', marginTop: '24px' }}>
-            Якщо ви не робили цей запит, просто ігноруйте цей лист.
-          </Text>
-        </Container>
-      </Body>
-    </Html>
+          {code}
+        </div>
+        <p style={{ fontSize: '14px', color: '#a0aec0', marginTop: '24px' }}>
+          Якщо ви не робили цей запит, просто ігноруйте цей лист.
+        </p>
+      </div>
+    </div>
   );
 };
